46a8704a02d1d4a832f984a6e1969d7dd4559147
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-12-13 01:09:16 +0200

    Вирішив конфлікти, скорегував стилі

--- src/helpres/markup.js
+++ src/helpres/markup.js
@@ -1,21 +1,21 @@
 const createMarkupForChangableIcon = isFavorite => {
   const markupForIcon = isFavorite
-    ? '<svg class="ChangebleIcon" width="16" height="16"><use href="../public/oleksii-symbol-defs.svg#icon-trash"></use></svg>'
-    : '<div class="IconRatingContainer"><p class="RatingInfoCard">4.0</p><svg class="ChangebleIcon" width="18" height="18"><use href="../public/oleksii-symbol-defs.svg#icon-star"></use></svg></div>';
+    ? '<button type="button" class="remove-btn" id="remove-favorite-btn"><svg class="changeble-icon" width="16" height="16"><use href="../public/oleksii-symbol-defs.svg#icon-trash"></use></svg></button>'
+    : '<div class="icon-rating-container"><p class="rating-info-card">4.0</p><svg class="changeble-icon" width="18" height="18"><use href="../public/oleksii-symbol-defs.svg#icon-star"></use></svg></div>';
 
   return markupForIcon;
 };
 
 export const createInfoCardMarkup = (cardData, isFavorite = false) => {
-  const { name, burnedCalories, bodyPart, target } = cardData;
+  const { name, burnedCalories, bodyPart, target, _id } = cardData;
 
-  return `<li class="favorite-info-card">
+  return `<li class="favorite-info-card" data-id=${_id}>
       <div class="card-header">
         <div class="category-and-icon">
           <div class="category-tag"><p>Workout</p></div>
           ${createMarkupForChangableIcon(isFavorite)}
         </div>
-        <div class="card-title-container">
+        <div>
           <button type="button" class="start-btn">
             <p>Start</p>
             <svg width="16" height="16">
@@ -31,16 +31,16 @@ export const createInfoCardMarkup = (cardData, isFavorite = false) => {
         <h3 class="card-title">${name}</h3>
       </div>
       <ul class="card-info-list">
-        <li class="InfoListItem">
+        <li>
           <p class="item-text">
-            Burned calories: <em class="InfoData">${burnedCalories}/3<strong>min</strong></em>
+            Burned calories: <em class="hidden-overflow-text">${burnedCalories}/3<strong>min</strong></em>
           </p>
         </li>
-        <li class="InfoListItem">
-          <p class="item-text">Body part: <em class="InfoData">${bodyPart}</em></p>
+        <li>
+          <p class="item-text">Body part: <em>${bodyPart}</em></p>
         </li>
-        <li class="InfoListItem">
-          <p class="item-text">Target: <em class="InfoData">${target}</em></p>
+        <li>
+          <p class="item-text">Target: <em>${target}</em></p>
         </li>
       </ul>
     </li>`;
--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,13 +1,25 @@
-import { getFromLocal } from '../services/local-storage';
+import {
+  getFromLocal,
+  removeFavoriteCardFromLocal,
+  saveToLocal,
+} from '../services/local-storage';
 import { getExercises } from '../services/api';
 import { createInfoCardMarkup } from '../helpres/markup';
 
-const list = document.querySelector('.favorite-cards-thumb');
+const favoritesList = document.getElementById('favorite-cards-list');
 
-console.log(list);
-getExercises().then(data => {
+console.log(favoritesList);
+await getExercises().then(data => {
   console.log(data.results[0]);
   const test = createInfoCardMarkup(data.results[0]);
-  list.insertAdjacentHTML('beforebegin', test);
+  favoritesList.insertAdjacentHTML('beforeend', test);
 });
-// export const favoriteCardsFromLS = getFromLocal();
+
+const handleDeleteFavoriteCard = ({ target }) => {
+  if (!target.closest('#remove-favorite-btn')) return;
+  const id = target.closest('[data-id="64f389465ae26083f39b17b7"]').dataset.id;
+  console.log(id);
+  removeFavoriteCardFromLocal(id);
+};
+
+favoritesList.addEventListener('click', handleDeleteFavoriteCard);
