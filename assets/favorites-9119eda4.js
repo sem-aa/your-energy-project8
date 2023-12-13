ecd3262de4df0c363bb870c03927779b59b90a7e
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-12-13 16:34:26 +0000

    Deploying to gh-pages from @ sem-aa/your-energy-project8@f09b6c1b08aaaff0048dc05a753978efb6883d06 🚀

---
+++ assets/favorites-9119eda4.js
@@ -0,0 +1,36 @@
+import{a}from"./vendor-a61d8330.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerpolicy&&(s.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?s.credentials="include":t.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(t){if(t.ep)return;t.ep=!0;const s=n(t);fetch(t.href,s)}})();a.defaults.baseURL="https://your-energy.b.goit.study/api/";const l=async({bodypart:r="",muscles:e="",equipment:n="",keyword:o="",page:t=1,limit:s=10}={})=>{try{return(await a.get(`exercises?bodypart=${r}&muscles=${e}&equipment=${n}&keyword=${o}&page=${t}&limit=${s}`)).data}catch(i){console.error("Error in getExercises:",i)}},v=async r=>{try{return(await a.get(`exercises/${r}`)).data}catch(e){console.error("Error in getExerciseId:",e)}},m=async r=>{try{return await a.post("subscription",r)}catch(e){return console.error("Error in subscribe:",e),e.response}},d=r=>r?'<button type="button" class="remove-btn" id="remove-favorite-btn"><svg class="changeble-icon" width="16" height="16"><use href="../public/oleksii-symbol-defs.svg#icon-trash"></use></svg></button>':'<div class="icon-rating-container"><p class="rating-info-card">4.0</p><svg class="changeble-icon" width="18" height="18"><use href="../public/oleksii-symbol-defs.svg#icon-star"></use></svg></div>',u=(r,e=!1)=>{const{name:n,burnedCalories:o,bodyPart:t,target:s,_id:i}=r;return`<li class="favorite-info-card" data-id=${i}>
+      <div class="card-header">
+        <div class="category-and-icon">
+          <div class="category-tag"><p>Workout</p></div>
+          ${d(e)}
+        </div>
+        <div>
+          <button type="button" class="start-btn">
+            <p>Start</p>
+            <svg width="16" height="16">
+              <use href="../public/oleksii-symbol-defs.svg#icon-arrow"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="card-title-container">
+         <svg width="24" height="24">
+          <use href="../public/oleksii-symbol-defs.svg#icon-man"></use>
+        </svg>
+        <h3 class="card-title">${n}</h3>
+      </div>
+      <ul class="card-info-list">
+        <li>
+          <p class="item-text">
+            Burned calories: <em class="hidden-overflow-text">${o}/3<strong>min</strong></em>
+          </p>
+        </li>
+        <li>
+          <p class="item-text">Body part: <em>${t}</em></p>
+        </li>
+        <li>
+          <p class="item-text">Target: <em>${s}</em></p>
+        </li>
+      </ul>
+    </li>`},f=r=>{const e=getFromLocal(KEY_STORAGE.favorites);if(!e){alert("Oops! Refresh page");return}const n=e.filter(({_id:o})=>o!==r);saveToLocal(KEY_STORAGE.favorites,n)},c=document.getElementById("favorite-cards-list");console.log(c);l().then(r=>{console.log(r.results[0]);const e=u(r.results[0]);c.insertAdjacentHTML("beforeend",e)});const g=({target:r})=>{if(!r.closest("#remove-favorite-btn"))return;const e=r.closest('[data-id="64f389465ae26083f39b17b7"]').dataset.id;console.log(e),f(e)};c.addEventListener("click",g);export{v as g,m as s};
+//# sourceMappingURL=favorites-9119eda4.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,16 +1,16 @@
-import{g as _}from"./assets/favorites-41103e7c.js";import"./assets/vendor-a61d8330.js";const a={openModalButton:document.querySelector(".open-modal"),modalExercises:document.querySelector(".modal-exercises")};function p(){a.openModalButton&&a.modalExercises?document.addEventListener("DOMContentLoaded",function(){a.openModalButton.addEventListener("click",e),a.modalExercises.addEventListener("click",l)}):console.error("One or more elements not found. Check your HTML and selectors.");function e(){a.modalExercises.classList.remove("visually-hidden")}function l(s){(s.target.closest(".modal-exercises__button-close")||s.target===a.modalExercises)&&a.modalExercises.classList.add("visually-hidden")}}function u(e){document.querySelectorAll("#rating .modal-exercises__rating-star").forEach(s=>{const t=parseInt(s.dataset.value,10);let i=0;t<=e?i=100:t-e<1&&(i=(e-t+1)*100),s.style.background=`linear-gradient(to right, gold ${i}%, gray ${i}%)`,s.style.webkitBackgroundClip="text",s.style.color="transparent"})}p();const x=document.querySelector(".modal-exercises");async function v(){try{const e=await _("64f389465ae26083f39b17a2");x.innerHTML=g(e),u(e.rating)}catch(e){console.error(e)}}function g(e){const{name:l,burnedCalories:s,bodyPart:t,description:i,target:r,equipment:o,gifUrl:c,popularity:d,rating:n,time:m,_id:f}=e;return`<div class="modal-exercises__card">
+import{s as _,g as p}from"./assets/favorites-9119eda4.js";import"./assets/vendor-a61d8330.js";const r=document.querySelector(".footer-form");r.addEventListener("submit",v);function v(e){e.preventDefault();const t={email:e.target.elements.email.value};r.reset(),_(t).then(s=>{switch(s.status){case 201:alert(s.data.message);break;case 409:alert(s.data.message);break;default:alert("Sorry, something went wrong((");break}})}const a={openModalButton:document.querySelector(".open-modal"),modalExercises:document.querySelector(".modal-exercises")};function x(){a.openModalButton&&a.modalExercises?document.addEventListener("DOMContentLoaded",function(){a.openModalButton.addEventListener("click",e),a.modalExercises.addEventListener("click",t)}):console.error("One or more elements not found. Check your HTML and selectors.");function e(){a.modalExercises.classList.remove("visually-hidden")}function t(s){(s.target.closest(".modal-exercises__button-close")||s.target===a.modalExercises)&&a.modalExercises.classList.add("visually-hidden")}}function g(e){document.querySelectorAll("#rating .modal-exercises__rating-star").forEach(s=>{const i=parseInt(s.dataset.value,10);let l=0;i<=e?l=100:i-e<1&&(l=(e-i+1)*100),s.style.background=`linear-gradient(to right, gold ${l}%, gray ${l}%)`,s.style.webkitBackgroundClip="text",s.style.color="transparent"})}x();const b=document.querySelector(".modal-exercises");async function f(){try{const e=await p("64f389465ae26083f39b17a2");b.innerHTML=h(e),g(e.rating)}catch(e){console.error(e)}}function h(e){const{name:t,burnedCalories:s,bodyPart:i,description:l,target:o,equipment:c,gifUrl:d,popularity:n,rating:m,time:u,_id:y}=e;return`<div class="modal-exercises__card">
     <button type="button" class="modal-exercises__button-close">
       <svg class="modal-exercises__button-icon" width="12" height="12">
         <use href="./images/icons.svg#icon-button-close"></use>
       </svg>
     </button>
     <div class="modal-exercises__image-wrapper">
-      <img class="modal-exercises__image" src="${c}" alt="${l}" />
+      <img class="modal-exercises__image" src="${d}" alt="${t}" />
     </div>
     <div class="modal-exercises__description">
-      <p class="modal-exercises__name">${l}</p>
+      <p class="modal-exercises__name">${t}</p>
       <div id="rating" class="modal-exercises__rating">
-        <div class="modal-exercises__rating-value">${n}</div>
+        <div class="modal-exercises__rating-value">${m}</div>
         <span class="modal-exercises__rating-star" data-value="1">&#9733;</span>
         <span class="modal-exercises__rating-star" data-value="2">&#9733;</span>
         <span class="modal-exercises__rating-star" data-value="3">&#9733;</span>
@@ -20,27 +20,27 @@ import{g as _}from"./assets/favorites-41103e7c.js";import"./assets/vendor-a61d83
       <div class="modal-exercises__partials">
         <div class="modal-exercises__partials-item">
           <p class="modal-exercises__partials-title">Target</p>
-          <p class="modal-exercises__partials-value">${r}</p>
+          <p class="modal-exercises__partials-value">${o}</p>
         </div>
         <div class="modal-exercises__partials-item">
           <p class="modal-exercises__partials-title">Body Part</p>
-          <p class="modal-exercises__partials-value">${t}</p>
+          <p class="modal-exercises__partials-value">${i}</p>
         </div>
         <div class="modal-exercises__partials-item">
           <p class="modal-exercises__partials-title">Equipment</p>
-          <p class="modal-exercises__partials-value">${o}</p>
+          <p class="modal-exercises__partials-value">${c}</p>
         </div>
         <div class="modal-exercises__partials-title">
           <p class="modal-exercises__partials-title">Popular</p>
-          <p class="modal-exercises__partials-value">${d}</p>
+          <p class="modal-exercises__partials-value">${n}</p>
         </div>
         <div class="modal-exercises__partials-item">
           <p class="modal-exercises__partials-title">Burned calories</p>
-          <p class="modal-exercises__partials-value">${s}/${m} min</p>
+          <p class="modal-exercises__partials-value">${s}/${u} min</p>
         </div>
       </div>
       <p class="modal-exercises__text">
-        ${i}
+        ${l}
       </p>
       <div class="modal-exercises__buttons">
         <button type="submit" class="modal-exercises__button-favourites">
@@ -56,5 +56,5 @@ import{g as _}from"./assets/favorites-41103e7c.js";import"./assets/vendor-a61d83
         <button class="modal-exercises__button-rating">Give a rating</button>
       </div>
     </div>
-  </div>`}v();
+  </div>`}f();
 //# sourceMappingURL=commonHelpers2.js.map
