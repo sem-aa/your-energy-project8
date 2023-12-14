b9338dd6e571b64dc5da8236a29433f6beb6c44d
nataliia <nataliia.goit@gmail.com>
2023-12-14 08:08:12 +0200

    add share-button

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import './categories';
 import './favorites';
 import './sorted-selected';
 import './searchParams';
-
+import './share-button';
 import './subscription';
 
 import { modalExercises } from './modal-exercises';
