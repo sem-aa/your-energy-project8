64d472ef0ba345ccb81ec83000c60ea1dc5e744a
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-15 22:02:12 +0200

    Export for loader

--- src/js/partials/loader.js
+++ src/js/partials/loader.js
@@ -1,9 +1,9 @@
 const loader = document.querySelector('.js-loader');
 
-function showLoader() {
+export function showLoader() {
   loader.classList.remove('.is-hidden');
 }
 
-function hideLoader() {
+export function hideLoader() {
   loader.classList.add('.is-hidden');
 }
