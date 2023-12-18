9e36de6db96f7514a2397c03af67376e76d8bf84
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-12-18 15:43:45 +0000

    Deploying to gh-pages from @ sem-aa/your-energy-project8@46ff2694c6dad0ac3387043e134e515353b289b5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{g as e,K as o}from"./assets/modal-rating-1149e4ff.js";import"./assets/vendor-eee4493b.js";const r=document.querySelector(".quote-text-fav"),u=document.querySelector(".quote-author-fav"),t=e(o.phrase);r.textContent=t.quote;u.textContent=t.author;
+import{g as e,K as o}from"./assets/modal-rating-f5f9c06a.js";import"./assets/vendor-eee4493b.js";const r=document.querySelector(".quote-text-fav"),u=document.querySelector(".quote-author-fav"),t=e(o.phrase);r.textContent=t.quote;u.textContent=t.author;
 //# sourceMappingURL=commonHelpers.js.map
